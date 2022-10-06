359e0afca1ff15d691d13ecdb4f4a465e2965b02
wilfred <=>
2022-10-07 00:41:33 +0300

    cross origin

--- src/main/java/com/wilfred/emsbackend/emsbackend/controller/EmployeeController.java
+++ src/main/java/com/wilfred/emsbackend/emsbackend/controller/EmployeeController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000/")
 @RequestMapping("/api/v1/")
 public class EmployeeController {
     private EmployeeService employeeService;
